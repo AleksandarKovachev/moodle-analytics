9db0847f5678ab65bb358149b201089f8e625982
AleksandarKovachev <akovachev@tu-sofia.bg>
2019-12-24 18:25:01 +0200

    Initial commit.

---
+++ src/main/java/com/moodle/analytics/MoodleAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.moodle.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodleAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodleAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moodle/analytics/MoodleAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodle.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodleAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
